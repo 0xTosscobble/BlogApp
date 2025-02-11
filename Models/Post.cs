c93b81b53c1f544ef4545426b96a7f853bb12963
Nicolas Rey <96reynic55@gmail.com>
2025-02-10 17:42:12 -0800

    Generated Post model and added necessary fields.

---
+++ Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogApp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string BlogPostTitle { get; set; }
+        public string BlogPostContent { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
